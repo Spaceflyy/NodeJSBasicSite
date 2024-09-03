6499a62fbe2dab7ffa87471d4e4e09b538632afc
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2024-09-03 16:20:30 +0100

    add 404 page

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ const url = require("node:url");
 
 const HOSTNAME = "localhost";
 const PORT = 8080;
+let statusCode = 200;
 
 const server = createServer((req, res) => {
 	let q = url.parse(req.url, true);
 	let filename = `.${q.pathname}`;
-	if (filename === "./") {
-		filename = "./index.html";
+
+	switch (req.url) {
+		case "/":
+			filename = "./index.html";
+			break;
+
+		case "/about.html":
+			filename = "./about.html";
+			break;
+
+		case "/contact-me.html":
+			filename = "./contact-me.html";
+			break;
+
+		default:
+			filename = "./404.html";
+			statusCode = 404;
+			break;
 	}
 
 	fs.readFile(filename, (err, data) => {
-		if (err) {
-			res.writeHead(404, { "Content-Type": "text/html" });
-			res.write("404");
-			res.end();
-		} else {
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(data);
-			res.end();
-		}
+		res.writeHead(statusCode, { "Content-Type": "text/html" });
+		res.write(data);
+		res.end();
 	});
 }).listen(8080, "localhost", () => {
 	console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
