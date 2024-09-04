cec962e3bf4774cdbc121e9151322de8c3de904d
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2024-09-04 15:37:03 +0100

    converted to express

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const { createServer } = require("node:http");
-const fs = require("fs");
-const url = require("node:url");
+const express = require("express");
+const app = express();
 
-const HOSTNAME = "localhost";
-const PORT = 8080;
-let statusCode = 200;
-
-const server = createServer((req, res) => {
-	let q = url.parse(req.url, true);
-	let filename = `.${q.pathname}`;
-
-	switch (req.url) {
-		case "/":
-			filename = "./index.html";
-			break;
-
-		case "/about.html":
-			filename = "./about.html";
-			break;
-
-		case "/contact-me.html":
-			filename = "./contact-me.html";
-			break;
-
-		default:
-			filename = "./404.html";
-			statusCode = 404;
-			break;
-	}
-
-	fs.readFile(filename, (err, data) => {
-		res.writeHead(statusCode, { "Content-Type": "text/html" });
-		res.write(data);
-		res.end();
-	});
-}).listen(8080, "localhost", () => {
-	console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/about", (req, res) => res.sendFile(__dirname + "/about.html"));
+app.get("/contact-me", (req, res) =>
+	res.sendFile(__dirname + "/contact-me.html")
+);
+app.get("*", (req, res) => {
+	res.sendFile(__dirname + "/404.html", 404);
 });
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+// const { createServer } = require("node:http");
+// const fs = require("fs");
+// const url = require("node:url");
+
+// const HOSTNAME = "localhost";
+// const PORT = 8080;
+// let statusCode = 200;
+
+// const server = createServer((req, res) => {
+// 	let parsedUrl = url.parse(req.url, true);
+// 	let filename = `.${parsedUrl.pathname}`;
+
+// 	fs.readFile(filename, (err, data) => {
+// 		res.writeHead(statusCode, { "Content-Type": "text/html" });
+// 		res.write(data);
+// 		res.end();
+// 	});
+// }).listen(8080, "localhost", () => {
+// 	console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+// });
